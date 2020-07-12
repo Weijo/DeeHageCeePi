a9b67f50b9110ff04fba71b77234247c1c2d7ded
Weijo <tayweijie777@gmail.com>
2020-07-12 21:02:45 +0800

    added more stuff

---
+++ edit.sh
@@ -0,0 +1,13 @@
+sudo rm /etc/network/interfaces -rf
+
+echo 'auto eth0' > /etc/network/interfaces
+echo 'iface eth0 inet static' >> /etc/network/interfaces
+echo 'address 192.168.3.4' >> /etc/network/interfaces
+echo 'netmask 255.255.255.0' >> /etc/network/interfaces
+echo 'network 192.168.3.0' >> /etc/network/interfaces
+echo 'broadcast 192.168.3.255' >> /etc/network/interfaces
+echo 'gateway 192.168.3.1' >> /etc/network/interfaces
+echo 'dns-nameservers 192.168.3.1' >> /etc/network/interfaces
+
+sudo rm /etc/resolve.conf -rf
+sudo /etc/init.d/networking restart
